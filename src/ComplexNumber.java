e93216313d26aa5240185dabcdc4b8f90d4777b9
g21turbo <dg21stew@gmail.com>
2023-12-11 21:54:55 -0800

    added comments

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -16,20 +16,24 @@ public class ComplexNumber {
         return imaginary;
     }
 
+    // Adds the specified real and imaginary values to the current complex number
     public void add(double real, double imaginary) {
         this.real += real;
         this.imaginary += imaginary;
     }
 
+    // Adds the real and imaginary parts of another complex number (other) to the current complex number
     public void add(ComplexNumber other) {
         add(other.real, other.imaginary);
     }
 
+    // Subtracts the specified real and imaginary values from the current complex number
     public void subtract(double real, double imaginary) {
         this.real -= real;
         this.imaginary -= imaginary;
     }
 
+    // Subtracts the real and imaginary parts of another complex number (other) from the current complex number
     public void subtract(ComplexNumber other) {
         subtract(other.real, other.imaginary);
     }
